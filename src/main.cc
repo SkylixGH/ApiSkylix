d3a82da58f4b7b10f55b25f62e91fe0864d2bf36
XFaon <xfaonae@protonmail.com>
2022-08-18 16:18:59 -0400

    Create main.cc

---
+++ src/main.cc
@@ -0,0 +1,25 @@
+#include <exolix/stdio.h>
+#include "net/net.h"
+#include "net/db.h"
+
+using Logger::about, Logger::info();
+
+int main() {
+    Logger::about("Skylix API Backend Service");
+    Logger::wait("Connecting to database via information from './skylix/config.h'");
+  
+    db::initDb();
+  
+    Logger::success("Successfully connected to database from './skylix/config.h'");
+    Logger::wait("Creating server objects");
+  
+    net::initNet();
+  
+    Logger::success("Successfully setup server objects!");
+    Logger::wait("Starting server listeners. Gettings configurations from './skylix/config.h'");
+  
+    net::startNet();
+  
+    Logger::wait("API backend servers are now online! Check configuration './skylix/config.h'");
+    Logger::info("API backend server is fully ready");
+}
